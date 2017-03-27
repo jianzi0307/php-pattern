7c19cec51945c5f123cc397b1dcdbb7965818a69
jian <jianzi0307@qq.com>
2017-03-27 16:40:50 +0800

    bridge pattern

--- com/pattern/bridge/MainApp.php
+++ com/pattern/bridge/MainApp.php
@@ -10,10 +10,14 @@ $light1 = new \com\pattern\bridge\lights\impl\IncanLight();
 //水晶灯
 $light2 = new \com\pattern\bridge\lights\impl\CrystalLight();
 
-print '--------- 一般开关'.PHP_EOL;
+print '--------- 一般开关控制白炽灯'.PHP_EOL;
 $switch = new \com\pattern\bridge\switchs\BaseSwitch($light1);
 $switch->makeLight();
 
-print '--------- 遥控开关'.PHP_EOL;
+print '--------- 遥控开关控制水晶灯'.PHP_EOL;
 $remoteSwitch = new \com\pattern\bridge\switchs\sub\RemoteControllSwitch($light2);
 $remoteSwitch->makeRemoteLight(2);
+
+print '--------- 遥控开关控制白炽灯'.PHP_EOL;
+$remoteSwitch2 = new \com\pattern\bridge\switchs\sub\RemoteControllSwitch($light1);
+$remoteSwitch2->makeRemoteLight(2);
