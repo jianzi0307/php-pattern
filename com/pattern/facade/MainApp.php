534eb9eed2b3999f38ceb1c9e42c1f91478a0ab3
jian <jianzi0307@qq.com>
2017-03-29 09:45:50 +0800

    facade pattern.

--- com/pattern/facade/MainApp.php
+++ com/pattern/facade/MainApp.php
@@ -7,5 +7,22 @@
 
 require('../loader/autoload.php');
 
+/*
+ 不用facade模式的做法：
+$fish = new \com\pattern\facade\SourSweetFish();
+print "开始做糖醋鲤鱼...".PHP_EOL;
+$fish->prepare();
+$fish->step1();
+$fish->step2();
+$fish->step3();
+$fish->step4();
+$fish->step5();
+$fish->end();
+print "糖醋鲤鱼出锅...".PHP_EOL;
+*/
+
+/**
+ * 外观模式、门面模式
+ */
 $facade = new \com\pattern\facade\CookFacade();
 $facade->cookSourSweetFish();
