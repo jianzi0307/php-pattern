8497c5894a5b41229e8b71f2fc26b9d9b0a794c2
jian <jianzi0307@qq.com>
2017-03-29 09:42:17 +0800

    facade pattern.

---
+++ com/pattern/facade/CookFacade.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: jian0307@icloud.com
+ * Date: 2017/3/29
+ * Time: 9:25
+ */
+
+namespace com\pattern\facade;
+
+
+class CookFacade implements ICookFacade
+{
+    private $sourSweetFish;
+
+    function __construct()
+    {
+        $this->sourSweetFish = new SourSweetFish();
+    }
+
+    public function cookSourSweetFish(): void
+    {
+        print "开始做糖醋鲤鱼...".PHP_EOL;
+        $this->sourSweetFish->prepare();
+        $this->sourSweetFish->step1();
+        $this->sourSweetFish->step2();
+        $this->sourSweetFish->step3();
+        $this->sourSweetFish->step4();
+        $this->sourSweetFish->step5();
+        $this->sourSweetFish->end();
+        print "糖醋鲤鱼出锅...".PHP_EOL;
+    }
+}
---
+++ com/pattern/facade/MainApp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: jian0307@icloud.com
+ * Date: 2017/3/29
+ * Time: 9:40
+ */
+
+require('../loader/autoload.php');
+
+$facade = new \com\pattern\facade\CookFacade();
+$facade->cookSourSweetFish();
