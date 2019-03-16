37fb54cbe693790f07583390258478fe162afe2b
jianzi0307 <jianzi0307@qq.com>
2019-03-16 23:06:22 +0800

    插入排序

---
+++ src/algorithm/sorting/InsertionSort.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jianzi0307
+ * Date: 2019/3/16
+ * Time: 22:49
+ */
+
+namespace src\algorithm\sorting;
+
+
+class InsertionSort extends BubbleSort
+{
+    public static function sort($arr)
+    {
+        $size = count($arr);
+        for ($i = 1; $i < $size; $i++) {
+            $j = $i;
+            while ($j > 0 && $arr[$j - 1] > $arr[$j]) {
+                self::_swap($arr[$j - 1], $arr[$j]);
+                $j--;
+            }
+        }
+        return $arr;
+    }
+}
